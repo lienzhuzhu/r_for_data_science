61253234974115c3cd46b1bf878f94f05f1ea1d6
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-02-02 14:06:33 -0600

    learning mapping

--- whole_game_1-data-vis.R
+++ whole_game_1-data-vis.R
@@ -4,5 +4,13 @@ library(ggthemes)
 
 ggplot(
     data = penguins,
-    mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
-) + geom_point() + geom_smooth(method = "lm")
+    mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +     geom_point(mapping = aes(color = species, shape = species)) +
+        geom_smooth(method = "lm") +
+        labs(
+            title = "Body Mass and Flipper Length",
+            subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+            x = "Flipper Length (mm)", y = "Body Mass (g)",
+            color = "Species", shape = "Species"
+        ) +
+        scale_color_colorblind()
