ebbeb6148375c48dcafed3d13b023f73667b6028
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-02-04 20:04:44 -0600

    3-5 WIP

--- chapter-3/3-5.R
+++ chapter-3/3-5.R
@@ -11,5 +11,43 @@ flights |>
 
 flights |> 
     group_by(dest) |> 
-    slice_max(dep_delay) |>
+    slice_max(dep_delay, n = 1) |>
     relocate(dest, dep_delay)
+
+flights |> 
+    group_by(hour) |>
+    summarize(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) |>
+    ggplot(aes(x = hour, y = avg_dep_delay)) +
+        geom_smooth(na.rm = TRUE)
+
+flights |>
+    group_by(dest) |>
+    arrange(desc(arr_delay)) |> 
+    slice_max(arr_delay, n = -240) |>
+    relocate(arr_delay, dest, tailnum)
+
+flights |>
+    group_by(dest) |>
+    arrange(desc(arr_delay)) |> 
+    slice_max(arr_delay, n = -245) |>
+    relocate(arr_delay, dest, tailnum)
+
+flights |> 
+    filter(dest == "ABQ") |> 
+    arrange(desc(arr_delay)) |> 
+    relocate(arr_delay)
+
+flights |> 
+    filter(dest == "ABQ") |> 
+    arrange(desc(arr_delay)) |> 
+    relocate(arr_delay)
+
+flights |>
+    filter(dest == "ABQ") |>
+    distinct(dest, arr_delay) |> 
+    arrange(desc(arr_delay))
+
+flights |> 
+    filter(dest == "ABQ") |> 
+    arrange(desc(arr_delay)) |> 
+    relocate(arr_delay)
