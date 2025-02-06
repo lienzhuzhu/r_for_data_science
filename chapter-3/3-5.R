654c669f3849eed218d90db9ca9c36c96a422aee
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-02-06 01:37:29 -0600

    finished chapters 3 and 4. slice() functions seem to be poorly documented

--- chapter-3/3-5.R
+++ chapter-3/3-5.R
@@ -1,6 +1,9 @@
 library(nycflights13)
 library(tidyverse)
 
+
+# 1-5. -----------------
+
 flights |>
     group_by(carrier) |>
     summarize(
@@ -20,34 +23,53 @@ flights |>
     ggplot(aes(x = hour, y = avg_dep_delay)) +
         geom_smooth(na.rm = TRUE)
 
-flights |>
-    group_by(dest) |>
-    arrange(desc(arr_delay)) |> 
-    slice_max(arr_delay, n = -240) |>
-    relocate(arr_delay, dest, tailnum)
-
-flights |>
-    group_by(dest) |>
-    arrange(desc(arr_delay)) |> 
-    slice_max(arr_delay, n = -245) |>
-    relocate(arr_delay, dest, tailnum)
+flights |> 
+    group_by(dest) |> 
+    slice_max(arr_delay, n = 1) |>  # Shows the max from each dest
+    relocate(dest, arr_delay)
 
 flights |> 
-    filter(dest == "ABQ") |> 
-    arrange(desc(arr_delay)) |> 
-    relocate(arr_delay)
+    group_by(dest) |> 
+    slice_min(arr_delay, n = -1) |> # Removes the max from each dest
+    arrange(dest, desc(arr_delay)) |> 
+    relocate(dest, arr_delay)
 
-flights |> 
-    filter(dest == "ABQ") |> 
-    arrange(desc(arr_delay)) |> 
-    relocate(arr_delay)
+# 6. -------------------------------
+df <- tibble(
+    x = 1:5,
+    y = c("a", "b", "a", "a", "b"),
+    z = c("K", "K", "L", "L", "K")
+)
 
-flights |>
-    filter(dest == "ABQ") |>
-    distinct(dest, arr_delay) |> 
-    arrange(desc(arr_delay))
+# Expected: the data frame tibble but with a notice that 
+# the data is grouped by the y variable
+df |>
+    group_by(y)
 
-flights |> 
-    filter(dest == "ABQ") |> 
-    arrange(desc(arr_delay)) |> 
-    relocate(arr_delay)
+# Expected: rows should be sorted by the y variable, with
+# rows with a value of 'a' before those with 'b'
+df |> 
+    arrange(y)
+
+# Expected: two rows because y only has two levels, each row
+# should have a new variable called mean_x
+df |>
+    group_by(y) |>
+    summarize(mean_x = mean(x))
+
+# Expected: 3x2 frame with means of var x for each unique combo
+# Result: as expected, but with notice that the z grouping was dropped
+df |>
+    group_by(y, z) |>
+    summarize(mean_x = mean(x))
+
+# Expected: same as last one but without warning, and retained groups
+df |>
+    group_by(y, z) |>
+    summarize(mean_x = mean(x), .groups = "drop")
+
+# Retains the rows, adds to the data frame a new column
+# with averages of value x for each combo of y and z
+df |>
+    group_by(y, z) |>
+    mutate(mean_x = mean(x))
