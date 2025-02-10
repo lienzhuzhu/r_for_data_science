e9d98b116b7fb56a95b71dcff0a3f5a5c78b240f
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-02-09 21:14:17 -0600

    9-3-1 finished

--- chapter-9/9-2-1.R
+++ chapter-9/9-2-1.R
@@ -12,3 +12,42 @@ ggplot(mpg) +
 mpg |> 
     ggplot(aes(x = displ, y = hwy, color = displ < 5)) +
     geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+    geom_smooth(aes(color = drv), show.legend = TRUE, se = FALSE)
+
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy)) +
+    geom_point(size = 3) +
+    geom_smooth(se = FALSE)
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy)) +
+    geom_point(size = 3) +
+    geom_smooth(aes(group = drv), se = FALSE)
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy, color = drv)) +
+    geom_point(size = 3) +
+    geom_smooth(aes(group = drv), se = FALSE)
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy)) +
+    geom_point(size = 3, aes(color = drv)) +
+    geom_smooth(se = FALSE)
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy)) +
+    geom_point(size = 2, aes(color = drv)) +
+    geom_smooth(se = FALSE, aes(linetype = drv))
+
+mpg |> 
+    ggplot(aes(x = displ, y = hwy)) +
+    geom_point(
+        size = 5, color = "white"
+    ) +
+    geom_point(
+        aes(color = drv),
+        size = 2
+    )
